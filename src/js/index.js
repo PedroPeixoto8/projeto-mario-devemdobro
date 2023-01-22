29cbef6e318e8eb5526866dda4dfbe266b55d241
PedroPeixoto8 <123281940+PedroPeixoto8@users.noreply.github.com>
2023-01-22 00:11:24 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+/*
+objetivo1 quando o usuario clicar no botao de ver trailler, devemos abrir a modal com video do trailler
+objetivo2 quando usuario clicar no x devemos fechar o modal
+ */
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+}
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
+
